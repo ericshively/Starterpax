68159a679c6a8759bb7106192764ad2d5ba804ad
Eric Shively <eshively@uci.edu>
2024-02-21 13:59:29 -0800

    Update description, robots.txt, and sitemap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function RootPage() {
     <main className="flex flex-col items-center justify-between pt-4 md:p-4 lg:p-12 xl:p-20 md:text-lg lg:text-xl xl:text-2xl">
        <h1> What are the best items to get started? </h1>
        <br></br> &nbsp; <br></br>
-       <h2> Starter Pax is a curated collection of high value starter items, or &quot;starter packs&quot;, for various hobbies. </h2> <br></br>
+       <h2> Starter Pax is a curated collection of high value starter items, or &quot;starter packs&quot;, for various hobbies and needs. </h2> <br></br>
        <br></br> &nbsp; <br></br>
        <h3> Select one of the categories above </h3>
        <br></br> &nbsp; <br></br>
@@ -12,6 +12,7 @@ export default function RootPage() {
        Most importantly, we are looking towards the most value for the money, &quot;bang for the buck&quot;, so you can get started efficiently.<br></br>&nbsp; <br></br>
        Items aren&apos;t the cheapest in their category but they&apos;re neither the premium overpriced options.<br></br>
        When you view our starter packs, you can rest easy knowing you can immediately dive into a new hobby without feeling overwhelmed by the options. <br></br>
+       We have no affiliation or sponsorship with any of the items listed. <br></br>
     </main>
   );
 }
